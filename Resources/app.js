693b5ec7e3c2deb5a8b5ca45b8795dc9ed4b6bae
Spolk4 <suzanne.m.polk@gmail.com>
2019-07-23 18:34:26 -0500

    First commit alienjs

--- Resources/app.js
+++ Resources/app.js
@@ -6,8 +6,25 @@ var filter_button = d3.select("#filter-btn");
 tableData.forEach(function (ufo){
     var tr_row = t_body.append("tr");
 
-    Object.defineProperties(ufo).forEach(finction([key, value]){
+    Object.defineProperties(ufo).forEach(function([key, value]){
         var td_cell = t_body.append("td")
         td_cell.text(value);
+    })
+});
+
+filter_button.on("click", function () {
+    var element =d3.select("#datetime");
+    var input =element.property("value");
+    t_body.html("")
+
+    var filtered = tableData.filter(elem=> elem.datetime ===input);
+    d3.event.preventDefault();
+    filtered.forEach(function (elem) {
+        var tr_row =t_body.append("tr");
+
+        Object.defineProperties(elem).forEach(function([key, value]){
+            var td_cell = t_body.append("td")
+            td_cell.text(value);
+        })
     })
 });
